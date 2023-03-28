6301c3af1cebc36fa6263549fcbd83cd23f06b48
artur893 <arthal893@gmail.com>
2023-03-28 17:42:13 +0200

    Password generator algorithm

--- src/modules/HomeModule/pages/HomePage/HomePage.tsx
+++ src/modules/HomeModule/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./HomePage.scss";
 
 interface IProps {
@@ -7,6 +7,52 @@ interface IProps {
 
 export const HomePage: React.FC<IProps> = ({ msg }) => {
   const [count, setCount] = useState(0);
+  const [password, setPassword] = useState<string>()
+
+  const passParams = {
+    length: 20,
+    passSource: {
+      lowerLetters: true,
+      upperLetters: true,
+      numbers: true,
+      symbols: true
+    }
+  }
+
+  const lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  const upperLetters = lowerLetters.map(elem => elem.toUpperCase())
+  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+  const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?']
+
+  let availableSigns: string[][] = []
+
+  function getRandomOne(collection: string | string[] | string[][]) {
+    return collection[Math.floor(Math.random() * collection.length)]
+  }
+
+  function createAvailableSigns() {
+    const updatedSigns = []
+    if (passParams.passSource.lowerLetters) { updatedSigns.push(lowerLetters) }
+    if (passParams.passSource.upperLetters) { updatedSigns.push(upperLetters) }
+    if (passParams.passSource.numbers) { updatedSigns.push(numbers) }
+    if (passParams.passSource.symbols) { updatedSigns.push(symbols) }
+    availableSigns = updatedSigns
+  }
+
+
+  function generatePassword() {
+    const password = []
+    for (let i = 1; i <= passParams.length; i++) {
+      password.push(getRandomOne(getRandomOne(availableSigns)))
+    }
+
+    setPassword(password.join(''))
+  }
+
+  useEffect(() => {
+    createAvailableSigns()
+    generatePassword()
+  }, [])
 
   return (
     <>
@@ -22,6 +68,7 @@ export const HomePage: React.FC<IProps> = ({ msg }) => {
         >
           count is {count}
         </button>
+        <p>password is {password}</p>
       </div>
     </>
   );
