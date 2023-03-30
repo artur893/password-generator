a0c28aea0d20de6ba8897d5dbc764a42b55c596d
artur893 <arthal893@gmail.com>
2023-03-30 09:46:05 +0200

    Slider works

---
+++ src/modules/HomeModule/components/PasswordGeneratorComponent/PasswordGeneratorComponent.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import "./PasswordGeneratorComponent.scss";
+
+export const PasswordGenerator: React.FC = () => {
+    const [password, setPassword] = useState<string>()
+
+    const initParams = {
+        length: 6,
+        passSource: {
+            lowerLetters: true,
+            upperLetters: true,
+            numbers: true,
+            symbols: true
+        }
+    }
+
+    const [passParams, setPassParams] = useState(initParams)
+    const [passLen, setPassLen] = useState(6)
+
+    const lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const upperLetters = lowerLetters.map(elem => elem.toUpperCase())
+    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?']
+
+    let availableSigns: string[][] = []
+
+    function getRandomOne(collection: string | string[] | string[][]) {
+        return collection[Math.floor(Math.random() * collection.length)]
+    }
+
+    function createSignsCollections() {
+        const updatedSigns = []
+        if (passParams.passSource.lowerLetters) { updatedSigns.push(lowerLetters) }
+        if (passParams.passSource.upperLetters) { updatedSigns.push(upperLetters) }
+        if (passParams.passSource.numbers) { updatedSigns.push(numbers) }
+        if (passParams.passSource.symbols) { updatedSigns.push(symbols) }
+        availableSigns = updatedSigns
+    }
+
+
+    function generatePassword() {
+        const password = []
+        createSignsCollections()
+        for (let i = 1; i <= passLen; i++) {
+            password.push(getRandomOne(getRandomOne(availableSigns)))
+        }
+
+        setPassword(password.join(''))
+    }
+
+    useEffect(() => {
+        generatePassword()
+    }, [passLen])
+
+    const changePassLenght = (event: any) => {
+        setPassLen(event.target.value);
+    };
+
+
+    return (
+        <div className="password-generator">
+            <h1 className="password-generator__title">Password Generator</h1>
+            <form className="password-generator__form">
+                <div className="password-generator__input-container">
+                    <div className="password-generator__range-label"><label>Character Length</label><span>{passLen}</span></div>
+                    <input type="range" min="4" max="16" step="1" value={passLen} onChange={changePassLenght} />
+                </div>
+                {/* <div className="password-generator__input-container">
+                    <label><input type="checkbox" checked />Include Uppercase Letter</label>
+                </div>
+                <div className="password-generator__input-container">
+                    <label><input type="checkbox" />Include Lowercase Letter</label>
+                </div>
+                <div className="password-generator__input-container">
+                    <label><input type="checkbox" checked />Include Numbers</label>
+                </div>
+                <div className="password-generator__input-container">
+                    <label><input type="checkbox" checked />Include Symbols</label>
+                </div> */}
+            </form>
+            <div className="password-generator__strength">TU BEDZIE SIŁA HASŁA</div>
+            <div className="password-generator__password">{password} <button onClick={generatePassword}>GENERATE</button></div>
+            <button className="password-generator__copy-button">Copy Password</button>
+        </div>
+    );
+};
--- src/modules/HomeModule/pages/HomePage/HomePage.tsx
+++ src/modules/HomeModule/pages/HomePage/HomePage.tsx
@@ -1,75 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./HomePage.scss";
+import { PasswordGenerator } from '../../components/PasswordGeneratorComponent/PasswordGeneratorComponent'
 
-interface IProps {
-  msg: string;
-}
-
-export const HomePage: React.FC<IProps> = ({ msg }) => {
-  const [count, setCount] = useState(0);
-  const [password, setPassword] = useState<string>()
-
-  const passParams = {
-    length: 20,
-    passSource: {
-      lowerLetters: true,
-      upperLetters: true,
-      numbers: true,
-      symbols: true
-    }
-  }
-
-  const lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  const upperLetters = lowerLetters.map(elem => elem.toUpperCase())
-  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-  const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?']
-
-  let availableSigns: string[][] = []
-
-  function getRandomOne(collection: string | string[] | string[][]) {
-    return collection[Math.floor(Math.random() * collection.length)]
-  }
-
-  function createAvailableSigns() {
-    const updatedSigns = []
-    if (passParams.passSource.lowerLetters) { updatedSigns.push(lowerLetters) }
-    if (passParams.passSource.upperLetters) { updatedSigns.push(upperLetters) }
-    if (passParams.passSource.numbers) { updatedSigns.push(numbers) }
-    if (passParams.passSource.symbols) { updatedSigns.push(symbols) }
-    availableSigns = updatedSigns
-  }
-
-
-  function generatePassword() {
-    const password = []
-    for (let i = 1; i <= passParams.length; i++) {
-      password.push(getRandomOne(getRandomOne(availableSigns)))
-    }
-
-    setPassword(password.join(''))
-  }
-
-  useEffect(() => {
-    createAvailableSigns()
-    generatePassword()
-  }, [])
+export const HomePage: React.FC = () => {
 
   return (
-    <>
-      <h1>{msg}</h1>
-      <div className="card">
-        <p>
-          See the README.md for more information on how to start your challenge.
-        </p>
-        <button
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>password is {password}</p>
-      </div>
-    </>
+    <div className="homepage">
+      <PasswordGenerator />
+    </div>
   );
 };
